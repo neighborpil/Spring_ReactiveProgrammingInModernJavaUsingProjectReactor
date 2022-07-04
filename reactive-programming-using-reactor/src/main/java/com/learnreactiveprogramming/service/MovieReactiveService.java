7f19e09c2021f4e76826cdd8cb0405268985c225
Neighborpil <neighborpil@naver.com>
2022-07-04 09:50:59 +0900

    retry and repeat

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/MovieReactiveService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/MovieReactiveService.java
@@ -4,10 +4,16 @@ import com.learnreactiveprogramming.domain.Movie;
 import com.learnreactiveprogramming.domain.MovieInfo;
 import com.learnreactiveprogramming.domain.Review;
 import com.learnreactiveprogramming.exception.MovieException;
+import com.learnreactiveprogramming.exception.NetworkException;
+import com.learnreactiveprogramming.exception.ServiceException;
+import java.time.Duration;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.Exceptions;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+import reactor.util.retry.RetryBackoffSpec;
 
 @Slf4j
 public class MovieReactiveService {
@@ -35,7 +41,58 @@ public class MovieReactiveService {
                 throw new MovieException(ex.getMessage());
             })
             .log();
+    }
 
+    public Flux<Movie> getAllMovies_retry() {
+
+        Flux<MovieInfo> movieInfoFlux = movieInfoService.retrieveMoviesFlux();
+        return movieInfoFlux
+            .flatMap(movieInfo -> {
+                Mono<List<Review>> reviewsMono = reviewService.retrieveReviewsFlux(
+                        movieInfo.getMovieInfoId())
+                    .collectList();
+                return reviewsMono.map(reviewsList -> new Movie(movieInfo, reviewsList));
+            })
+            .onErrorMap((ex) -> {
+                log.error("Exception is : " + ex);
+                throw new MovieException(ex.getMessage());
+            })
+//            .retry() // retry over and over again
+            .retry(3)
+            .log();
+    }
+
+    public Flux<Movie> getAllMovies_retryWhen() {
+
+        Flux<MovieInfo> movieInfoFlux = movieInfoService.retrieveMoviesFlux();
+        return movieInfoFlux
+            .flatMap(movieInfo -> {
+                Mono<List<Review>> reviewsMono = reviewService.retrieveReviewsFlux(
+                        movieInfo.getMovieInfoId())
+                    .collectList();
+                return reviewsMono.map(reviewsList -> new Movie(movieInfo, reviewsList));
+            })
+            .onErrorMap((ex) -> {
+                log.error("Exception is : " + ex);
+                if (ex instanceof NetworkException) {
+                    throw new MovieException(ex.getMessage());
+                } else {
+                    throw new ServiceException(ex.getMessage());
+                }
+
+            })
+            .retryWhen(getRetryBackoffSpec())
+            .log();
+    }
+
+    private RetryBackoffSpec getRetryBackoffSpec() {
+        //        RetryBackoffSpec retryWhen = Retry.backoff(3, Duration.ofMillis(500))
+//            .onRetryExhaustedThrow(((retryBackoffSpec, retrySignal) -> Exceptions.propagate(retrySignal.failure())));
+
+        RetryBackoffSpec retryWhen = Retry.fixedDelay(3, Duration.ofMillis(500))
+            .filter(ex -> ex instanceof MovieException) // MovieException일때만 재시도한다
+            .onRetryExhaustedThrow(((retryBackoffSpec, retrySignal) -> Exceptions.propagate(retrySignal.failure())));
+        return retryWhen;
     }
 
     public Mono<Movie> getMovieById(long movieId) {
@@ -44,6 +101,54 @@ public class MovieReactiveService {
         Mono<List<Review>> reviewList = reviewService.retrieveReviewsFlux(movieId).collectList();
 
         return movieInfoMono.zipWith(reviewList, (movieInfo, reviews) -> new Movie(movieInfo, reviews));
+    }
+
+    public Flux<Movie> getAllMovies_repeat() {
+
+        Flux<MovieInfo> movieInfoFlux = movieInfoService.retrieveMoviesFlux();
+        return movieInfoFlux
+            .flatMap(movieInfo -> {
+                Mono<List<Review>> reviewsMono = reviewService.retrieveReviewsFlux(
+                        movieInfo.getMovieInfoId())
+                    .collectList();
+                return reviewsMono.map(reviewsList -> new Movie(movieInfo, reviewsList));
+            })
+            .onErrorMap((ex) -> {
+                log.error("Exception is : " + ex);
+                if (ex instanceof NetworkException) {
+                    throw new MovieException(ex.getMessage());
+                } else {
+                    throw new ServiceException(ex.getMessage());
+                }
+
+            })
+            .retryWhen(getRetryBackoffSpec())
+            .repeat() // 같은 작업을 2번 반복한다
+            .log();
+    }
+
+    public Flux<Movie> getAllMovies_repeat_n(long n) {
+
+        Flux<MovieInfo> movieInfoFlux = movieInfoService.retrieveMoviesFlux();
+        return movieInfoFlux
+            .flatMap(movieInfo -> {
+                Mono<List<Review>> reviewsMono = reviewService.retrieveReviewsFlux(
+                        movieInfo.getMovieInfoId())
+                    .collectList();
+                return reviewsMono.map(reviewsList -> new Movie(movieInfo, reviewsList));
+            })
+            .onErrorMap((ex) -> {
+                log.error("Exception is : " + ex);
+                if (ex instanceof NetworkException) {
+                    throw new MovieException(ex.getMessage());
+                } else {
+                    throw new ServiceException(ex.getMessage());
+                }
+
+            })
+            .retryWhen(getRetryBackoffSpec())
+            .repeat(n) // 같은 작업을 n번 반복한다
+            .log();
     }
 
     public Mono<Movie> getMovieById2(long movieId) {
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/MovieReactiveServiceMockTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/MovieReactiveServiceMockTest.java
@@ -2,10 +2,15 @@ package com.learnreactiveprogramming.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import com.learnreactiveprogramming.domain.Movie;
 import com.learnreactiveprogramming.exception.MovieException;
+import com.learnreactiveprogramming.exception.NetworkException;
 import com.learnreactiveprogramming.exception.ReactorException;
+import com.learnreactiveprogramming.exception.ServiceException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -61,5 +66,112 @@ class MovieReactiveServiceMockTest {
 //            .expectError(MovieException.class)
             .expectErrorMessage(errorMessage)
             .verify();
+    }
+
+    @Test
+    void getAllMovies_retry() {
+
+        var errorMessage = "Exception occurred";
+
+        Mockito.when(movieInfoService.retrieveMoviesFlux())
+            .thenCallRealMethod();
+
+        Mockito.when(reviewService.retrieveReviewsFlux(anyLong()))
+            .thenThrow(new RuntimeException(errorMessage));
+
+        Flux<Movie> moviesFlux = reactiveMovieService.getAllMovies_retry();
+
+        StepVerifier.create(moviesFlux)
+//            .expectError(MovieException.class)
+            .expectErrorMessage(errorMessage)
+            .verify();
+
+        verify(reviewService, times(4)).retrieveReviewsFlux(isA(Long.class));
+    }
+
+    @Test
+    void getAllMovies_retryWhen_ServiceException() {
+
+        var errorMessage = "Exception occurred";
+
+        Mockito.when(movieInfoService.retrieveMoviesFlux())
+            .thenCallRealMethod();
+
+        Mockito.when(reviewService.retrieveReviewsFlux(anyLong()))
+            .thenThrow(new ServiceException(errorMessage));
+
+        Flux<Movie> moviesFlux = reactiveMovieService.getAllMovies_retryWhen();
+
+        StepVerifier.create(moviesFlux)
+//            .expectError(MovieException.class)
+            .expectErrorMessage(errorMessage)
+            .verify();
+
+        verify(reviewService, times(1)).retrieveReviewsFlux(isA(Long.class));
+    }
+    @Test
+    void getAllMovies_retryWhen_NetworkException() {
+
+        var errorMessage = "Exception occurred";
+
+        Mockito.when(movieInfoService.retrieveMoviesFlux())
+            .thenCallRealMethod();
+
+        Mockito.when(reviewService.retrieveReviewsFlux(anyLong()))
+            .thenThrow(new NetworkException(errorMessage));
+
+        Flux<Movie> moviesFlux = reactiveMovieService.getAllMovies_retryWhen();
+
+        StepVerifier.create(moviesFlux)
+//            .expectError(MovieException.class)
+            .expectErrorMessage(errorMessage)
+            .verify();
+
+        verify(reviewService, times(4)).retrieveReviewsFlux(isA(Long.class));
+    }
+
+
+    @Test
+    void getAllMovies_repeat() {
+        var errorMessage = "Exception occurred";
+
+        Mockito.when(movieInfoService.retrieveMoviesFlux())
+            .thenCallRealMethod();
+
+        Mockito.when(reviewService.retrieveReviewsFlux(anyLong()))
+            .thenCallRealMethod();
+
+        Flux<Movie> moviesFlux = reactiveMovieService.getAllMovies_repeat();
+
+        StepVerifier.create(moviesFlux)
+//            .expectError(MovieException.class)
+            .expectNextCount(6)
+            .thenCancel()
+            .verify();
+
+        verify(reviewService, times(6))
+            .retrieveReviewsFlux(isA(Long.class));
+    }
+    @Test
+    void getAllMovies_repeat_n() {
+        var errorMessage = "Exception occurred";
+
+        Mockito.when(movieInfoService.retrieveMoviesFlux())
+            .thenCallRealMethod();
+
+        Mockito.when(reviewService.retrieveReviewsFlux(anyLong()))
+            .thenCallRealMethod();
+
+        var noOfTimes = 2L;
+
+        Flux<Movie> moviesFlux = reactiveMovieService.getAllMovies_repeat_n(noOfTimes);
+
+        StepVerifier.create(moviesFlux)
+//            .expectError(MovieException.class)
+            .expectNextCount(9)
+            .verifyComplete();
+
+        verify(reviewService, times(9))
+            .retrieveReviewsFlux(isA(Long.class));
     }
 }
